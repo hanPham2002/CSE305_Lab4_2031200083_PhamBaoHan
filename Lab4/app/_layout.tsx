1344d676366b19b633c8adeb20e2e4f1f009aad6
hanPham2002 <phambaohan160402@gmail.com>
2025-06-20 22:44:53 +0700

    update

---
+++ Lab4/app/_layout.tsx
@@ -0,0 +1,38 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+import store from "@/Store";
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { Provider } from "react-redux";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar />
+
+      </ThemeProvider>
+    </Provider>
+  );
+}
